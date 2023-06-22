820d0832e2c4c95287566d6497be69c74c9ae7ce
Moshe Gitlin <jmoshe052@gmail.com>
2023-06-22 10:33:27 +0300

    app update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState} from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Routes as Switch, Route } from 'react-router-dom';
@@ -9,16 +9,17 @@ import Login from "./components/pages/Login";
 import './App.css';
 
 function App() {
+    const [isAuthenticated,setIsAuthenticated] = useState(false)
   return (
       <>
         <Router>
-          <Navbar />
+          <Navbar isAuthenticated = {isAuthenticated} />
           <Switch>
             <Route path='/'  element={<Home/>} />
             <Route path='/services' element={<Services/>} />
             <Route path='/products' element={<Products/>} />
             <Route path='/sign-up' element={<SignUp/>} />
-            <Route path='/log-in' element={<Login/>} />
+            <Route path='/log-in' element={<Login setIsAuthenticated = {setIsAuthenticated}/>} />
           </Switch>
         </Router>
       </>
