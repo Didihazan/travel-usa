abe0f10fa05837f16d3431e8f46232be61390dc7
Didihazan <109093124+Didihazan@users.noreply.github.com>
2023-06-25 21:39:39 +0300

    Update Services.js

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import '../../App.css';
+import Layout from "../Layout";
 
 export default function Services() {
-    return <h1 className='services'>SERVICES</h1>;
+    return(
+        <Layout>
+        <h1 className='services'>SERVICES</h1>
+        </Layout>
+            );
 }
