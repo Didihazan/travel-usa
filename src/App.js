f8acd9a6982cab637149db9b465304ffae4dd937
Moshe Gitlin <jmoshe052@gmail.com>
2023-06-25 09:28:26 +0300

    db update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import Services from './components/pages/Services';
 import SignUp from './components/pages/SignUp';
 import Login from "./components/pages/Login";
 import './App.css';
-import Stays from "./components/pages/Stays";
+import BookingForm from "./components/pages/Stays";
+
 
 function App() {
     const [isAuthenticated,setIsAuthenticated] = useState(false)
@@ -18,7 +19,7 @@ function App() {
           <Switch>
             <Route path='/'  element={<Home/>} />
             <Route path='/services' element={<Services/>} />
-            <Route path='/products' element={<Stays/>} />
+            <Route path='/products' element={<BookingForm/>} />
             <Route path='/sign-up' element={<SignUp/>} />
             <Route path='/log-in' element={<Login setIsAuthenticated = {setIsAuthenticated} user ={user} setuser={setUser}/>} />
           </Switch>
