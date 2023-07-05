b05a6cd6d6a2a0d8b8f62c22420eb12b66264309
Moshe Gitlin <jmoshe052@gmail.com>
2023-07-05 10:56:18 +0300

    update server

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,4 @@
-// export function Button() {
-//   return (
-//     <Link to='sign-up'>
-//       <button className='btn'>Sign Up</button>
-//     </Link>
-//   );
-// }
 
-;
 import './Button.css';
 import { Link } from 'react-router-dom';
 
