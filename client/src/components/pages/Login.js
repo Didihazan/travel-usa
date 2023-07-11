32edb932ee3dbaf97a92f167ef405b87e8d615c4
Didihazan <109093124+Didihazan@users.noreply.github.com>
2023-07-11 23:42:32 +0300

    Update Login.js

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,31 +1,39 @@
-
-import {useNavigate } from "react-router-dom";
-import '../Login.css'
-import '../../App.css';
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import {FaFacebook, FaGoogle, FaMobileAlt} from "react-icons/fa";
+import "../Login.css";
+import "../../App.css";
 
 
 export default function Login(prop) {
-    let navigate = useNavigate()
-    const HandleLogin = (event)=>{
-        const name = event.target[0].value
-        const pass = event.target[1].value
+    let navigate = useNavigate();
+    const [rememberMe, setRememberMe] = useState(false);
 
-        let url ="http://localhost:3001/users"
-        fetch(url).then((result)=> {return result.json()}).then((data)=>{
-            data.forEach(user =>{
-                if (user.password === pass && user.userName === name){
-                    prop.setIsAuthenticated(true)
-                    prop.setuser(user)
-                }
-            })
-        })
-        if (prop.user){
-            return navigate("/")
+    const HandleLogin = (event) => {
+        event.preventDefault();
+        const name = event.target[0].value;
+        const pass = event.target[1].value;
+
+        let url = "http://localhost:3001/users";
+        fetch(url)
+            .then((result) => result.json())
+            .then((data) => {
+                data.forEach((user) => {
+                    if (user.password === pass && user.userName === name) {
+                        prop.setIsAuthenticated(true);
+                        prop.setUser(user);
+                    }
+                });
+            });
+
+        if (prop.user) {
+            return navigate("/");
         }
-        event.preventDefault()
+    };
 
-    }
-
+    const handleRememberMe = () => {
+        setRememberMe(!rememberMe);
+    };
 
     return (
 
@@ -41,10 +49,34 @@ export default function Login(prop) {
                     placeholder="Password.."
                     className="login-input"
                 />
-                <button type="submit" className="login-button" >
+                <div className="remember-me">
+                    <input
+                        type="checkbox"
+                        id="rememberMe"
+                        checked={rememberMe}
+                        onChange={handleRememberMe}
+                    />
+                    <label htmlFor="rememberMe">Remember me</label>
+                </div>
+                <button type="submit" className="login-button">
                     Log In
                 </button>
             </form>
+            <div className="login-options">
+                <hr className="line"/>
+                <span className="or-text">or use one of these options</span>
+                <div className="login-icons">
+                    <div className="icon-container">
+                        <FaFacebook className="icon"/>
+                    </div>
+                    <div className="icon-container">
+                        <FaGoogle className="icon"/>
+                    </div>
+                    <div className="icon-container">
+                        <FaMobileAlt className="icon"/>
+                    </div>
+                </div>
+            </div>
         </div>
 
     );
