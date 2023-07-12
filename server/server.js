c00bc5a7db9879858ec8697e9f0f74e0e06c1835
Moshe Gitlin <jmoshe052@gmail.com>
2023-07-12 08:39:16 +0300

    update

--- server/server.js
+++ server/server.js
@@ -1,5 +1,13 @@
 const http = require('http')
 const app = require('./app')
-const port = 3001
+const debug = require('debug')('express1:server')
+const port = (process.env.PORT ||'3001')
+
+
+app.set('port',port)
 const server = http.createServer(app)
 server.listen(port)
+
+server.on('error',onerror)
+server.on('listening',onListening)
+
