993ff02d8f106896e6f54c434c793033fb78d54c
Moshe Gitlin <jmoshe052@gmail.com>
2023-07-24 19:09:15 +0300

    update app

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import {BrowserRouter as Router, Routes as Switch, Route} from 'react-router-dom';
+// import {useNavigate} from 'react-router-dom';
 import Services from './components/pages/Services';
 import BookingForm from "./components/pages/Stays";
 import Gallery from "./components/pages/Gallery";
@@ -10,21 +11,25 @@ import Login from "./components/pages/Login";
 import './App.css';
 
 
+
 function App() {
+
     const [isAuthenticated, setIsAuthenticated] = useState(false)
     const [user, setUser] = useState({})
     return (
         <>
             <Router>
-                <Navbar isAuthenticated={isAuthenticated}/>
+                <Navbar isAuthenticated={isAuthenticated}
+                        setIsAuthenticated={setIsAuthenticated}
+                        setuser={setUser}/>
                 <Switch>
                     <Route path='/' element={<Home/>}/>
-                    <Route path='/services' element={<Services/>}/>
-                    <Route path='/stays' element={<BookingForm/>}/>
-                    <Route path='/gallery' element={<Gallery/>}/>
+                    <Route path='/services' element={isAuthenticated?<Services/>:<Login/>}/>
+                    <Route path='/stays' element={isAuthenticated?<BookingForm/>:<Login/>}/>
+                    <Route path='/gallery' element={isAuthenticated?<Gallery/>:<Login/>}/>
                     <Route path='/sign-up' element={<SignUp />}/>
                     <Route path='/log-in'
-                           element={<Login setIsAuthenticated={setIsAuthenticated} user={user} setuser={setUser}/>}/>
+                           element={<Login setIsAuthenticated={setIsAuthenticated}  setuser={setUser}/>}/>
                 </Switch>
             </Router>
         </>
