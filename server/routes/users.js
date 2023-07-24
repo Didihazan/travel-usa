bf979acce2cd6f2fa6a142510ad099b741c279c5
Moshe Gitlin <jmoshe052@gmail.com>
2023-07-24 19:34:21 +0300

    update user route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const {signup,
-  login}=require('../controllers/users')
+  login,getId}=require('../controllers/users')
+const checkAuth = require("../middlewares/checkAuth");
 
 
 router.post('/signup',signup)
 router.post('/login',login)
+router.get('/token',checkAuth,getId)
 
 module.exports = router
