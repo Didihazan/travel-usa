18104555840c8491759bbc98c6dff3eed2e99263
Didihazan <109093124+Didihazan@users.noreply.github.com>
2023-07-26 10:32:14 +0300

    Add files via upload

---
+++ client/src/components/pages/Orders.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../Layout';
+import '../Orders.css';
+
+export default function Orders() {
+    return (
+        <Layout>
+<div className="mainOrder">
+
+
+
+</div>
+        </Layout>
+    );
+}
