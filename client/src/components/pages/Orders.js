645adf82455c4e58d46631dbce581c4ac5af7319
Didihazan <109093124+Didihazan@users.noreply.github.com>
2023-08-16 19:58:13 +0300

    Update Orders.js

--- client/src/components/pages/Orders.js
+++ client/src/components/pages/Orders.js
@@ -1,43 +1,81 @@
 import React from 'react';
 import Layout from '../Layout';
 import '../Orders.css';
-import {RiHotelBedLine} from "react-icons/ri";
+import { RiHotelBedLine } from "react-icons/ri";
 import moment from "moment/moment";
-import {FaPlane} from "react-icons/fa";
+import {
+    FaDumbbell,
+    FaPlane,
+    FaSwimmingPool,
+    FaTree,
+    FaUtensils,
+    FaWifi,
+} from "react-icons/fa";
 
 export default function Orders(props) {
-
-    function handleClick() {
-        console.log(props.checkVacation._id)
-    }
+    function handleClick() {}
 
     return (
         <Layout>
-<div className="mainOrder">
-    <div className="picture">
-        <img src={`http://localhost:3001/images/${props.checkVacation.imageSrc}`} alt="Destination Picture" />
-    </div>
-    <div className="content">
-        <h2>{props.checkVacation.name_place}</h2>
-        <div className="title">
-            <span className="price">{props.checkVacation.price}</span>
-            <span className="discount">{props.checkVacation.discount}</span>
-        </div>
-        <div className="hotel">
-            <RiHotelBedLine />
-            <span className="dates"> {moment(props.checkVacation.check_in).format('DD-MM-YYYY')} -> {moment(props.checkVacation.check_out).format('DD-MM-YYYY')}</span>
-        </div>
-        <div className="flight">
-            <FaPlane />
-            <span className="details">{props.checkVacation.Flight_Details}</span>
-        </div>
-        <span className="details">{props.checkVacation.Parking && <h1>{props.checkVacation._id}</h1>}</span>
-        <button onClick={handleClick}>save</button>
-        <button>pay</button>
-    </div>
+            <div className="mainOrder">
+                <div className="orderContent">
+                    <h2 className="orderTitle">{props.checkVacation.name_place}</h2>
+                    <div className="orderPrice">
+            <span className="discountedPrice">
+              {props.checkVacation.price}
+            </span>
+                        <span className="originalPrice">
+              {props.checkVacation.discount}
+            </span>
+                    </div>
+                    <div className="iconRow">
+                        <div className="iconColumnLeft">
+                            <div className="iconWithCaption">
+                                <FaPlane className="icon" />
+                                <span className="caption">Flight</span>
+                            </div>
+                            <div className="iconWithCaption">
+                                <RiHotelBedLine className="icon" />
+                                <span className="caption">Hotel</span>
+                            </div>
+                            <div className="iconWithCaption">
+                                <FaTree className="icon" />
+                                <span className="caption">Park</span>
+                            </div>
+                            <div className="iconWithCaption">
+                                <FaSwimmingPool className="icon" />
+                                <span className="caption">Pool</span>
+                            </div>
+                        </div>
+                        <div className="iconColumnRight">
 
+                            <div className="iconWithCaption">
+                                <FaWifi className="icon" />
+                                <span className="caption">WIFI</span>
+                            </div>
+                            <div className="iconWithCaption">
+                                <FaUtensils className="icon" />
+                                <span className="caption">Restaurant</span>
+                            </div>
+                            <div className="iconWithCaption">
+                                <FaDumbbell className="icon" />
+                                <span className="caption">Gym</span>
+                            </div>
+                        </div>
+                    </div>
 
-</div>
+                    <div className="buttonsRow">
+                        <button onClick={handleClick}>Add to Cart</button>
+                        <button>Pay</button>
+                    </div>
+                </div>
+                <div className="orderPicture">
+                    <img
+                        src={`http://localhost:3001/images/${props.checkVacation.imageSrc}`}
+                        alt="Destination Picture"
+                    />
+                </div>
+            </div>
         </Layout>
     );
 }
