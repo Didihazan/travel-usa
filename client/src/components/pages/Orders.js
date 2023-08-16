4b2efdac5f5a337d506465c087975f6cfed99ad6
Moshe Gitlin <jmoshe052@gmail.com>
2023-08-16 20:15:22 +0300

    all

--- client/src/components/pages/Orders.js
+++ client/src/components/pages/Orders.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Layout from '../Layout';
 import '../Orders.css';
 import {RiHotelBedLine} from "react-icons/ri";
 import moment from "moment/moment";
 import {FaPlane} from "react-icons/fa";
+import axios from "axios";
 
 export default function Orders(props) {
 
-    function handleClick() {
+    const saveOrder= async ()=> {
         console.log(props.checkVacation._id)
+
+        const Data = {
+
+            userId:props.user,
+            cardId:props.checkVacation._id
+        }
+        try {
+             await axios.post("http://localhost:3001/order/add",Data);
+        } catch (error){
+            console.error(error);
+        }
+
     }
+    const deleteOrder = async ()=>{
+
+    }
+    const getOrders = async ()=>{
+        const res= await axios.get(`http://localhost:3001/order/${props.user}`)
+        const data = res.data.orders;
+        console.log(data)
+    }
+    useEffect(()=>{
+        getOrders()
+    },[props.checkVacation !== null])
 
     return (
         <Layout>
-<div className="mainOrder">
-    <div className="picture">
+            {props.checkVacation &&<div className="mainOrder">
+   <div className="picture">
         <img src={`http://localhost:3001/images/${props.checkVacation.imageSrc}`} alt="Destination Picture" />
     </div>
     <div className="content">
@@ -32,12 +56,12 @@ export default function Orders(props) {
             <span className="details">{props.checkVacation.Flight_Details}</span>
         </div>
         <span className="details">{props.checkVacation.Parking && <h1>{props.checkVacation._id}</h1>}</span>
-        <button onClick={handleClick}>save</button>
+        <button onClick={saveOrder}>save</button>
+        <button onClick={deleteOrder}>delete</button>
         <button>pay</button>
     </div>
 
-
-</div>
+</div>}
         </Layout>
     );
 }
